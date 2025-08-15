52d619208531cf9e0636b490c5b8aaa49fbdc092
sam-l-112 <aa0926287193@email.com>
2025-08-16 03:37:31 +0800

    init ELK-sterss-test files

---
+++ tools/install_ansbile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Update package list
+sudo apt update
+
+# Install ansible
+sudo apt install -y ansible
+
+# Ensure Python venv module is installed
+# sudo apt install -y python3.12-venv
+
+# # Create Python virtual environment if not exists
+# if [ ! -d ".venv" ]; then
+#   python3 -m venv .venv
+# fi
+
+
+# 以下自行輸入
+# # Activate the virtual environment
+# source .venv/bin/activate
+
+# # Upgrade pip and install Ansible
+# pip install --upgrade pip
+# pip install ansible requests joblib tqdm
+
+# echo "Virtual environment and Ansible are ready."
