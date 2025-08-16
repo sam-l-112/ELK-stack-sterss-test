11be16cd718824ee8823f4e5b1cf0f4bf6bcb856
sam-l-112 <aa0926287193@email.com>
2025-08-17 03:46:50 +0800

    ELK-stack-sterss-test 分支內容更新

--- tools/install_ansbile.sh
+++ tools/install_ansbile.sh
@@ -13,13 +13,13 @@ sudo apt install -y ansible
 # Install additional tools
 sudo apt install -y tree
 
-# Ensure Python venv module is installed
-sudo apt install -y python3.12-venv
+# # Ensure Python venv module is installed
+# sudo apt install -y python3.12-venv
 
-# # Create Python virtual environment if not exists
-if [ ! -d ".venv" ]; then
-  python3 -m venv .venv
-fi
+# # # Create Python virtual environment if not exists
+# if [ ! -d ".venv" ]; then
+#   python3 -m venv .venv
+# fi
 
 
 # 以下自行輸入
